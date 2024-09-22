688672937f8fed23d28ce4bfeed5387707cc6298
Ivan Gomes <ivan-gomes@users.noreply.github.com>
2024-09-22 03:38:13 -0500

    build: increment version to 0.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.vanniktech.maven.publish.SonatypeHost
 
 group = "com.twingineer"
-version = "0.1.3-SNAPSHOT"
+version = "0.1.3"
 description = "Fluent, safe templating in 100% Kotlin."
 
 plugins {
